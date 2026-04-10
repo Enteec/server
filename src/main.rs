112207207a15b680d6498c55c9b1acc174397e96
Teeco123 <dastardly7761@proton.me>
2026-04-10 13:18:16 +0200

    feat: axum server with ping endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(ping));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to listen on IP:PORT!");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start a server!");
+}
+
+async fn ping() -> &'static str {
+    "pong"
 }
