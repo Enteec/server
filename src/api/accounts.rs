6ee1b21f3b0777c079ad657daf7e3952714d9afa
Enrok <enrok.gaming@gmail.com>
2026-04-15 00:25:59 +0200

    feat: checking email

--- src/api/accounts.rs
+++ src/api/accounts.rs
@@ -14,9 +14,31 @@ pub fn accounts_routes() -> Router {
 }
 
 async fn register(Json(input): Json<NewUserInput>) -> String {
-    format!("Received data: {} \nPassword confirm {}", input.name,pass_check(&input.password ,&input.password_confirm))
+    format!("Received data: {} \nPassword confirm {}\nEmail confirm {}", input.name,pass_check(&input.password ,&input.password_confirm),email_check(&input.email))
 }
 
 fn pass_check(password: &str, password_confirm: &str ) -> bool {
     password == password_confirm
+}
+
+fn email_check(email: &str) -> bool {
+    if email.contains(char::is_whitespace) {
+        return false;
+    }
+
+    if email.starts_with('@') || email.starts_with('.') {
+        return false;
+    }
+
+    if email.ends_with('@') || email.ends_with('.') {
+        return false;
+    }
+
+    let mut parts = email.split('@');
+
+    match (parts.next(), parts.next(), parts.next()) {
+        (Some(local), Some(domain), None)
+        if !local.is_empty() && !domain.starts_with('.') && domain.contains('.') => true,
+        _ => false,
+    }
 }
