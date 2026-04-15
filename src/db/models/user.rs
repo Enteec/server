bb6c0d10cd348398cdec72cacc334bea54f0724c
Teeco123 <dastardly7761@proton.me>
2026-04-15 02:03:12 +0200

    feat: added user new function and made models public

--- src/db/models/user.rs
+++ src/db/models/user.rs
@@ -1,5 +1,5 @@
 use crate::db::schema::users;
-use chrono::NaiveDateTime;
+use chrono::{NaiveDateTime, Utc};
 use diesel::{insert_into, prelude::*, result::Error};
 use uuid::Uuid;
 
@@ -14,6 +14,20 @@ pub struct User {
     pub name: String,
     pub password_hash: String,
     pub salt: String,
+}
+
+impl User {
+    pub fn new(name: &str, password: &str, salt: &str) -> Self {
+        let now = Utc::now().naive_utc();
+        Self {
+            uuid: uuid::Uuid::new_v4(),
+            created_at: now,
+            updated_at: now,
+            name: name.to_string(),
+            password_hash: password.to_string(),
+            salt: salt.to_string(),
+        }
+    }
 }
 
 impl User {
