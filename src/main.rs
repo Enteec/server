80679e104244f95b41bd475fbb47ac1cb841074e
Teeco <dastardly7761@proton.me>
2026-05-08 14:54:46 +0000

    feat: working websocket connection with receiving and sending message (#10)
    * feat: added feature ws for axum
    
    * feat: working websocket connection
    
    * fix: lint error fix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ use crate::{
     api::api_routes,
     db::{DbPool, create_pool},
 };
-use axum::Router;
+
+use axum::{
+    Router,
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::any,
+};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -21,6 +27,7 @@ async fn main() {
 
     let app = Router::new()
         .nest("/api", api_routes())
+        .route("/ws", any(ws_handler))
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
@@ -30,4 +37,27 @@ async fn main() {
     axum::serve(listener, app)
         .await
         .expect("Failed to start a server!");
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        match msg {
+            Message::Text(text) => {
+                println!("Received: {text}");
+                if socket
+                    .send(Message::Text("Hello from server!".into()))
+                    .await
+                    .is_err()
+                {
+                    break;
+                }
+            }
+            Message::Close(_) => break,
+            _ => (),
+        }
+    }
 }
