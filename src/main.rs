181db155678ca8fffd24f975d01e2daf205d9276
Teeco <dastardly7761@proton.me>
2026-05-10 19:51:37 +0200

    feat: logging and tracking (#11)
    * feat: added logging packages
    
    * feat: created tracing subscriber
    
    * feat: log listening ip port
    
    * feat: error display & error enum value contains other value
    
    * feat: logging in register function
    
    * fix: lint error fix

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use axum::{
     response::Response,
     routing::any,
 };
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -21,6 +23,14 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env()
+                .or_else(|_| EnvFilter::try_new("info"))
+                .unwrap(),
+        )
+        .init();
+
     let pool = create_pool();
 
     let app_state = AppState { db_pool: pool };
@@ -33,6 +43,7 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .expect("Failed to listen on IP:PORT!");
+    info!("Listening on http://{}", listener.local_addr().unwrap());
 
     axum::serve(listener, app)
         .await
